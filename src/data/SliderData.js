0440d3951aab6f6836b9c0c32976e65d9b0c5061
Mateusz <mateuszmikla@wp.pl>
2021-08-03 12:32:28 +0200

    small changes 03.08.2021

--- src/data/SliderData.js
+++ src/data/SliderData.js
@@ -6,17 +6,12 @@ export const SliderData = [
   {
     id: 1,
     title: "Dekoracje ślubne",
-    // price: "125,000$",
-    // path: "/homes",
-    label: "O mnie",
     image: ImageOne,
     alt: "Dekoracje Ślubne",
   },
   {
     id: 2,
     title: "Kompozycje kwiatowe",
-    // price: "99,000$",
-    // path: "/homes",
     label: "",
     image: ImageTwo,
     alt: "Kompozycje kwiatowe",
@@ -24,8 +19,6 @@ export const SliderData = [
   {
     id: 3,
     title: "Dekoracje okolicznościowe",
-    // price: "113,000$",
-    // path: "/homes",
     label: "Kontakt",
     image: ImageThree,
     alt: "Dekoracje okolicznościowe",
